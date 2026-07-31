db97027e4c6e052d741deb605a2cc71106c2f6de
Kevin Duncke <kevinduncke@gmail.com>
2026-07-31 00:46:47 -0300

    package.json update + minimal changes

--- netlink-api/src/routes/privacy.ts
+++ netlink-api/src/routes/privacy.ts
@@ -10,10 +10,15 @@ import {
 
 const router = Router();
 
+// BLOCK
 router.post('/block/:id', authenticate, blockUser);
 router.delete('/block/:id', authenticate, unblockUser);
+
+// MUTE OR RESTRICT
 router.post('/mute/:id', authenticate, muteUser);
 router.delete('/mute/:id', authenticate, unmuteUser);
+
+// REPORT
 router.post('/report/:id', authenticate, reportUser);
 
 export default router;
