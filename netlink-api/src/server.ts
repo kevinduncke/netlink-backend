bcfa237f063e6dbdeb36e74fa052b8d21fee7128
Kevin Duncke <kevinduncke@gmail.com>
2026-07-31 01:48:01 -0300

    updated server.ts app.listen

--- netlink-api/src/server.ts
+++ netlink-api/src/server.ts
@@ -1,9 +1,9 @@
 import { createApp } from "./app";
 
-const PORT = process.env.PORT || 4400;
+const PORT = Number(process.env.PORT) || 4400;
 
 const app = createApp();
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`\nNETLINK API LISTENING ON PORT ${PORT}`);
 });
